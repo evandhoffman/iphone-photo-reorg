2564e7ee2c0c7b8bfc6ae883f7dd257cf08cc496
Evan Hoffman <evandhoffman@gmail.com>
2024-05-18 01:11:22 +0000

    move files

--- reorg.py
+++ reorg.py
@@ -1,6 +1,7 @@
 import exifread
 import os
 import logging
+from datetime import datetime
 
 FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.INFO)
@@ -8,10 +9,11 @@ logger = logging.getLogger(__name__)
 
 
 base_dir = '/lunix1/data1/public/Media/Photo Imports/'
+base_dir_target = '/lunix1/data1/public/Media/Gallery/Photos/'
 
 files = []
 
-whitelist_prefixes = ['IMG']
+whitelist_prefixes = ['IMG','DSC']
 whitelist_suffixes = ['JPG', 'HEIC']
 
 def file_list(root_dir):
@@ -24,10 +26,25 @@ def file_list(root_dir):
             for pre in whitelist_prefixes:
                 if fi.startswith(pre):
                     for suf in whitelist_suffixes:
-                        if fi.endswith(suf):
+                        if fi.lower().endswith(suf.lower()):
                             files.append(os.path.join(r, fi))
-                            logger.info(f'Added file {fi} to list')
+                            logger.debug(f'Added file {fi} to list')
                             continue
+
+# datetime format: 2023:10:28 13:05:29
+def move_file(source_file, timestamp):
+    f = datetime.strptime(str(timestamp), '%Y:%m:%d %H:%M:%S')
+    new_date_path = f.strftime('%Y/%Y-%m/')
+    try:
+        os.makedirs(f"{base_dir_target}{new_date_path}")
+        base_filename = os.path.basename(source_file)
+        new_filename = f"{base_dir_target}{new_date_path}{f.strftime('%Y-%m-%d')}.{base_filename}"
+        os.rename(source_file, new_filename)
+        logger.info(f"Move file {source_file} to date {new_filename}")
+        return True
+    except Exception as e:
+        logger.error(f'Unable to move file: {e}')
+        return False
 
 
 if __name__ == '__main__':
@@ -40,8 +57,11 @@ if __name__ == '__main__':
             try:
                 tags = exifread.process_file(fh, stop_tag='DateTimeOriginal')
                 for tag in tags.keys():
-                    if tag.lower().contains('date'):
-                        logger.info("Key: %s, value %s" % (tag, tags[tag]))
+                    if 'date' in tag.lower():
+                        logger.debug("Key: %s, value %s" % (tag, tags[tag]))
+                        if tag == 'EXIF DateTimeOriginal':
+                            move_file(f, tags[tag])
+
             except Exception as e:
                 logger.error(f'Unable to get tags from {f}: {e}')
                 continue
